71ac092482b3dd62a17956e9d68c394b0b759596
Michael Dodge <mjdodge@dodgeco.com>
2020-04-08 17:08:06 -0400

    unit tests

--- report_pdf_options/static/src/js/qwebactionmanager.js
+++ report_pdf_options/static/src/js/qwebactionmanager.js
@@ -20,26 +20,26 @@ odoo.define('pdf_report_options.report', function(require) {
     };
 
     ActionManager.include({
-        _showDialogPdfOption: function(action, options) {
-            var self = this;
-            var btnClose = function() {
-                $('#frame-pdf').remove();
-                this.close();
-            }
-            var $dialog = new Dialog(self, {
-                title: _t("What do you want to do?"),
-                size: 'medium',
-                $content: QWeb.render('print_attachment_options.button_options'),
-                buttons: [{ text: _t('Close'), click: btnClose }]
-            }).open();
-            $dialog.opened().then(function() {
-                $dialog.$('.btn-pdf').on('click', function(e) {
-                    self.pdfReportOption = e.currentTarget.dataset.optionPdf;
-                    //trigger the download of the PDF report
-                    return self._triggerDownload(action, options, 'pdf');
-                });
-            });
-        },
+//        _showDialogPdfOption: function(action, options) {
+//            var self = this;
+//            var btnClose = function() {
+//                $('#frame-pdf').remove();
+//                this.close();
+//            }
+//            var $dialog = new Dialog(self, {
+//                title: _t("What do you want to do?"),
+//                size: 'medium',
+//                $content: QWeb.render('print_attachment_options.button_options'),
+//                buttons: [{ text: _t('Close'), click: btnClose }]
+//            }).open();
+//            $dialog.opened().then(function() {
+//                $dialog.$('.btn-pdf').on('click', function(e) {
+//                    self.pdfReportOption = e.currentTarget.dataset.optionPdf;
+//                    //trigger the download of the PDF report
+//                    return self._triggerDownload(action, options, 'pdf');
+//                });
+//            });
+//        },
         _downloadReport: function (url) {
             var self = this;
             framework.blockUI();
@@ -69,37 +69,37 @@ odoo.define('pdf_report_options.report', function(require) {
                 }
             });
         },
-        _executeReportAction: function(action, options) {
-            var self = this;
-
-            if (action.report_type === 'qweb-html') {
-                return this._executeReportClientAction(action, options);
-            } else if (action.report_type === 'qweb-pdf') {
-                // check the state of wkhtmltopdf before proceeding
-                return this.call('report', 'checkWkhtmltopdf').then(function(state) {
-                    // display a notification according to wkhtmltopdf's state
-                    if (state in WKHTMLTOPDF_MESSAGES) {
-                        self.do_notify(_t('Report'), WKHTMLTOPDF_MESSAGES[state], true);
-                    }
-                    if (state === 'upgrade' || state === 'ok') {
-                        if (action.default_print_option) {
-                            self.pdfReportOption = action.default_print_option;
-                            return self._triggerDownload(action, options, 'pdf');
-                        } else {
-                            return self._showDialogPdfOption(action, options);
-                        }
-                    } else {
-                        // open the report in the client action if generating the PDF is not possible
-                        return self._executeReportClientAction(action, options);
-                    }
-                });
-            } else if (action.report_type === 'qweb-text') {
-                return self._triggerDownload(action, options, 'text');
-            } else {
-                console.error("The ActionManager can't handle reports of type " +
-                    action.report_type, action);
-                return Promise.reject();
-            }
+//        _executeReportAction: function(action, options) {
+//            var self = this;
+//
+//            if (action.report_type === 'qweb-html') {
+//                return this._executeReportClientAction(action, options);
+//            } else if (action.report_type === 'qweb-pdf') {
+//                // check the state of wkhtmltopdf before proceeding
+//                return this.call('report', 'checkWkhtmltopdf').then(function(state) {
+//                    // display a notification according to wkhtmltopdf's state
+//                    if (state in WKHTMLTOPDF_MESSAGES) {
+//                        self.do_notify(_t('Report'), WKHTMLTOPDF_MESSAGES[state], true);
+//                    }
+//                    if (state === 'upgrade' || state === 'ok') {
+//                        if (action.default_print_option) {
+//                            self.pdfReportOption = action.default_print_option;
+//                            return self._triggerDownload(action, options, 'pdf');
+//                        } else {
+//                            return self._showDialogPdfOption(action, options);
+//                        }
+//                    } else {
+//                        // open the report in the client action if generating the PDF is not possible
+//                        return self._executeReportClientAction(action, options);
+//                    }
+//                });
+//            } else if (action.report_type === 'qweb-text') {
+//                return self._triggerDownload(action, options, 'text');
+//            } else {
+//                console.error("The ActionManager can't handle reports of type " +
+//                    action.report_type, action);
+//                return Promise.reject();
+//            }
         }
     });
 });
